50302bd2efc923c75d6014309de87c1e6c89c67d
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2021-03-07 20:08:12 -0600

    Use mypy

--- tap_confluence/streams.py
+++ tap_confluence/streams.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
+
 from base64 import b64encode
 from pathlib import Path
-from typing import Any, Dict, Iterable, Optional
+from typing import Any, Dict, Iterable, List
 
 import requests
 
@@ -12,7 +14,7 @@ SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
 class TapConfluenceStream(RESTStream):
 
     limit: int = 100
-    expand = []
+    expand: List[str] = []
 
     @property
     def url_base(self) -> str:
@@ -31,7 +33,7 @@ class TapConfluenceStream(RESTStream):
 
         return result
 
-    def get_url_params(self, partition: Optional[dict]) -> Dict[str, Any]:
+    def get_url_params(self, partition: dict | None) -> Dict[str, Any]:
         return {"limit": self.limit, "expand": ",".join(self.expand)}
 
     def parse_response(self, response: requests.Response) -> Iterable[dict]:
@@ -46,7 +48,7 @@ class TapConfluenceStream(RESTStream):
         params["start"] = next_page
         return params
 
-    def request_records(self, partition: Optional[dict]) -> Iterable[dict]:
+    def request_records(self, partition: dict | None) -> Iterable[dict]:
         start = 0
         while True:
             prepared_request = self.prepare_request(partition, next_page_token=start)
